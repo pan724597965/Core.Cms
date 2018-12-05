48bf9cd930da3b7029822e9cb34944d54a8be82b
孔融送你一个梨 <onlylv_bb@foxmail.com>
2018-12-05 10:54:18 +0800

    Initial commit

---
+++ Pitos/Pitos/Controllers/ContentController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Pitos.Models;
+
+namespace Pitos.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        // GET: Content
+        public IActionResult Index()
+        {
+            //var contents = new List<Content>();
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            return View(new ContentViewModel { Contents= new List<Content> { contents } });
+        }
+
+        // GET: Content/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Content/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Content/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Content/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Content/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Content/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Content/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Pitos/Pitos/Models/Content.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pitos.Models
+{
+    /// <summary>
+    /// 2018.12.04
+    /// Panbb
+    /// 内容实体
+    /// </summary>
+    public class Content
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public int status { get; set; }
+        public DateTime add_time { get; set; }
+        public DateTime modify_time { get; set; }
+    }
+}
